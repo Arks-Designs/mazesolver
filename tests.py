247936dbbb5b2e903a9135620ecdaaabddc98e8c
JR <jrpatton1234@gmail.com>
2025-03-01 10:44:07 -0500

    Add test for outer walls except start and end

--- tests.py
+++ tests.py
@@ -26,5 +26,19 @@ class Tests(unittest.TestCase):
             self.assertFalse(m1._cells[0][0].has_top_wall)
             self.assertFalse(m1._cells[-1][-1].has_bottom_wall)
 
+    def test_outer_walls(self):
+        num_cols = 3
+        num_rows = 4
+        m1 = Maze(0,0, num_rows, num_cols, 10, 10)
+        m1._create_cells()
+        for i in range(num_cols):
+            if i != 0:
+                self.assertTrue(m1._cells[i][0].has_top_wall)
+            if i != num_cols - 1:
+                self.assertTrue(m1._cells[i][-1].has_bottom_wall)
+        for j in range(num_rows):
+            self.assertTrue(m1._cells[0][j].has_left_wall)
+            self.assertTrue(m1._cells[-1][j])
+
 if __name__ == "__main__":
     unittest.main()
